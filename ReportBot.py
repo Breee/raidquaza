8e0b1bc7b51cc522626b7be970889cf4efb8011a
Bree@Nox-vm <loeffler.uni@gmail.com>
2018-04-13 13:40:33 +0200

    simple command to collect and store messages
    First step to create training sets

--- ReportBot.py
+++ ReportBot.py
@@ -62,6 +62,7 @@ class ReportBot(commands.Bot):
         self.add_command(self.stats)
         self.add_command(self.exterminate)
         self.add_command(self.search)
+        self.add_command(self.getmessages)
         self.start_time = 0
         self.session = aiohttp.ClientSession(loop=self.loop)
         #
@@ -197,6 +198,20 @@ class ReportBot(commands.Bot):
         embed = discord.Embed(color=0xa80000, title="Top results for query '%s'" % query,
                               description=msg)
         await self.send_message(destination=ctx.message.channel, content="", embed=embed)
+
+    @commands.command(pass_context=True)
+    async def getmessages(self, ctx, number):
+        mgs = []  # Empty list to put all the messages in the log
+        number = int(number)  # Converting the amount of messages to delete to an integer
+        async for x in self.logs_from(ctx.message.channel, limit=number):
+            if x.author != self.user:
+                mgs.append(x)
+
+        with open("messages.log", "w") as message_file:
+            for message in mgs:
+                message_file.write(message.content + "\n")
+
+
 
 
     """
